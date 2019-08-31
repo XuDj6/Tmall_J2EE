d44248799816bb6ca4ea41fa544e2294f3d75f9b
Tmall <362124982@qq.com>
2019-08-31 22:10:44 +0800

    the second commit

---
+++ tmall/src/tmall/dao/PropertyValueDAO.java
@@ -0,0 +1,213 @@
+package tmall.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import tmall.bean.Property;
+import tmall.bean.PropertyValue;
+import tmall.util.DBUtil;
+
+public class PropertyValueDAO {
+	
+	public int getTotal() {
+		int total = 0;
+		try(Connection c = DBUtil.getConnection(); Statement s = c.createStatement();){
+			
+			String sql = "select count(*) from PropertyValue";
+			ResultSet rs = s.executeQuery(sql);
+			if(rs.next()) {
+				total = rs.getInt(0);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return total;
+	}
+	
+	public void add(PropertyValue bean) {
+		String sql = "insert into PropertyValue values(null,?,?,?,?)";
+		try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);){
+			
+			ps.setInt(1, bean.getProduct().getId());
+			ps.setInt(2, bean.getProperty().getId());
+			ps.setString(3,bean.getValue());
+			ps.execute();
+			ResultSet rs = ps.getGeneratedKeys();
+			if(rs.next()) {
+				bean.setId(rs.getInt(1));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update(PropertyValue bean) {
+		String sql = "update PropertyValue set pid=?, ptid=?, value=? where id=?";
+		try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);){
+			
+			ps.setInt(1, bean.getProperty().getId());
+			ps.setInt(2, bean.getProperty().getId());
+			ps.setString(3, bean.getValue());
+			ps.setInt(4, bean.getId());
+			ps.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void delete(int id) {
+		try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();){
+			
+			String sql = "delete from PropertyValue where id="+id;
+			s.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public PropertyValue get(int id) {
+		PropertyValue bean = new PropertyValue();
+		
+		try(Connection c = DBUtil.getConnection();Statement s = c.createStatement();){
+			
+			String sql = "select * from PropertyValue where id = "+id;
+			ResultSet rs = s.executeQuery(sql);
+			
+			if(rs.next()) {
+				int pid = rs.getInt("pid");
+				int ptid = rs.getInt("ptid");
+				String value = rs.getString("value");
+				
+//				Product product = new ProductDAO().get(pid);
+				Property property = new PropertyDAO().get(ptid);
+				bean.setId(id);
+//				bean.setProduct(product);
+				bean.setProperty(property);
+				bean.setValue(value);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return bean;
+	}
+	
+	public PropertyValue get(int ptid,int pid) {
+		PropertyValue bean = null;
+		try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();){
+			
+			String sql = "select * from PropertyValue where ptid="+ptid+" and pid="+pid;
+			ResultSet rs = s.executeQuery(sql);
+			
+			if(rs.next()) {
+				bean = new PropertyValue();
+				int id = rs.getInt("id");
+				String value = rs.getString("value");
+				bean.setId(id);
+//				bean.setProduct(new Prod);
+				bean.setProperty(new PropertyDAO().get(ptid));
+				bean.setValue(value);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return bean;
+	}
+	
+	public List<PropertyValue> list(){
+		return list(0,Short.MAX_VALUE);
+	}
+	
+	public List<PropertyValue> list(int start, int count){
+		List<PropertyValue> beans = new ArrayList<PropertyValue>();
+		String sql = "select * from PropertyValue order by id desc limit ?,?";
+		
+		try(Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);){
+			ps.setInt(1, start);
+			ps.setInt(2, count);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				PropertyValue bean = new PropertyValue();
+				int id = rs.getInt("id");
+				int pid = rs.getInt("pid");
+				int ptid = rs.getInt("ptid");
+				String value = rs.getString("value");
+//				Product product = new ProductDAO().get(pid);
+				Property property = new PropertyDAO().get(ptid);
+				
+				bean.setId(id);
+//				bean.setProduct(product);
+				bean.setProperty(property);
+				bean.setValue(value);
+				beans.add(bean);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return beans;
+	}
+	
+//	public void init(Product p) {
+//		List<Property> pts = new PropertyDAO().get(p.getP)
+//	}
+	
+	public List<PropertyValue> list(int pid) {
+        List<PropertyValue> beans = new ArrayList<PropertyValue>();
+         
+        String sql = "select * from PropertyValue where pid = ? order by ptid desc";
+  
+        try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
+  
+            ps.setInt(1, pid);
+  
+            ResultSet rs = ps.executeQuery();
+  
+            while (rs.next()) {
+                PropertyValue bean = new PropertyValue();
+                int id = rs.getInt(1);
+ 
+                int ptid = rs.getInt("ptid");
+                String value = rs.getString("value");
+                 
+//                Product product = new ProductDAO().get(pid);
+                Property property = new PropertyDAO().get(ptid);
+//                bean.setProduct(product);
+                bean.setProperty(property);
+                bean.setValue(value);
+                bean.setId(id);          
+                beans.add(bean);
+            }
+        } catch (SQLException e) {
+  
+            e.printStackTrace();
+        }
+        return beans;
+    }
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tmall/src/tmall/dao/ReviewDAO.java
@@ -0,0 +1,9 @@
+package tmall.dao;
+
+public class ReviewDAO {
+
+	public int getTotal() {
+		int total = 0;
+		try (Connection c = )
+	}
+}
